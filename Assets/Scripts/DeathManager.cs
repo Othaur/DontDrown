cf66fc41bf0cc5d54d0b68a5f37c5468baf421de
russell <russellnadin@gmail.com>
2025-01-26 11:04:35 -0600

    Creating death manager

---
+++ Assets/Scripts/DeathManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource soundsOfDying;
+    [SerializeField] private GameObject deathPanel;
+
+    private void Start()
+    {
+        deathPanel.SetActive(false);
+    }
+
+    public void Death()
+    {
+        soundsOfDying.Play();
+        deathPanel.SetActive(true);
+
+    }
+
+
+
+}
