18749f77734d723321bbb01b3e158f8ec1395ff2
russell <russellnadin@gmail.com>
2025-01-25 20:23:16 -0600

    AirBubble self destructs after 10 seconds.

--- Assets/Scripts/GiantAirBubble.cs
+++ Assets/Scripts/GiantAirBubble.cs
@@ -9,6 +9,7 @@ public class GiantAirBubble : MonoBehaviour
     [SerializeField] private float airSupplyIncrease;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float movementForce;
+    private float bubbleBurstCountdown = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +22,32 @@ public class GiantAirBubble : MonoBehaviour
         rb.AddForce(Vector3.up * movementForce);
     }
 
-
+    private void Update()
+    {
+        BubbleBurstCountdown();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        /*if (GameObject.FindObjectOfType<FPCController>())
+        if (GameObject.FindObjectOfType<FluidDynamicsController>())
         {
             //Increases player's air supply.
             air.ChangeAirSupply(airSupplyIncrease);
             StartCoroutine(BurstBubble());
-        }*/
+        }
+    }
+
+    private void BubbleBurstCountdown()
+    {
+        if (bubbleBurstCountdown > 0)
+        {
+            bubbleBurstCountdown -= Time.deltaTime;
+            //Debug.Log(bubbleBurstCountdown);
+        }
+        else
+        {
+            StartCoroutine(BurstBubble());
+        }
     }
 
     IEnumerator BurstBubble()
